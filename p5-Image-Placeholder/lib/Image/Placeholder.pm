b76315754a2616e6f8bbb89a107ccc29bb38a1c9
Mark Norman Francis <norm@cackhanded.net>
2010-10-31 23:14:26 +0000

    Command-line script to generate images.

--- p5-Image-Placeholder/lib/Image/Placeholder.pm
+++ p5-Image-Placeholder/lib/Image/Placeholder.pm
@@ -5,9 +5,30 @@ use Moose;
 use MooseX::Method::Signatures;
 use MooseX::FollowPBP;
 
+use GD;
+
+use constant TRUE_COLOUR      => 1;
+use constant MAX_TRANSPARENCY => 127;
+use constant MAX_USABLE_RATIO => 0.85;
+
+has background_colour => (
+    isa     => 'Str',
+    is      => 'ro',
+    default => 'ddd',
+);
+has font => (
+    isa     => 'Str',
+    is      => 'ro',
+    default => 'Museo Sans',
+);
 has height => (
     isa     => 'Int',
     is      => 'ro',
+);
+has line_colour => (
+    isa     => 'Str',
+    is      => 'ro',
+    default => '444',
 );
 has size => (
     isa => 'Str',
@@ -16,6 +37,16 @@ has size => (
 has text => (
     isa     => 'Str',
     is      => 'rw',
+);
+has text_colour => (
+    isa     => 'Str',
+    is      => 'ro',
+    default => '36f',
+);
+has transparent => (
+    isa     => 'Bool',
+    is      => 'ro',
+    default => 0,
 );
 has width => (
     isa     => 'Int',
@@ -37,6 +68,69 @@ method BUILD {
     
     $self->set_default_text()
         unless defined $self->get_text();
+    
+    $self->{'_image'} = GD::Image->new(
+            $self->get_width(),
+            $self->get_height(),
+            TRUE_COLOUR
+        );
+    $self->{'_image'}->saveAlpha(1);
+    $self->{'_image'}->alphaBlending(1);
+    $self->{'_image'}->useFontConfig(1);
+    
+    $self->{'_line'} = $self->allocate_colour( $self->get_line_colour() );
+    $self->{'_background'} = $self->allocate_colour(
+            $self->get_background_colour(),
+            $self->get_transparent() * MAX_TRANSPARENCY
+        );
+    $self->{'_text'} = $self->allocate_colour(
+            $self->get_text_colour(),
+            int( MAX_TRANSPARENCY * 0.6 )
+        );
+}
+
+method generate {
+    my $image = $self->{'_image'};
+    my $line  = $self->{'_line'};
+    my $x     = $self->get_width() - 1;
+    my $y     = $self->get_height() - 1;
+    
+    # draw the border and cross
+    $image->fill( 0, 0, $self->{'_background'} );
+    
+    if ( $self->get_line_colour ne 'none' ) {
+        $image->setAntiAliased( $line );
+        $image->line( 0, 0, $x, $y, gdAntiAliased );
+        $image->line( $x, 0, 0, $y, gdAntiAliased );
+        $image->rectangle( 0, 0, $x, $y, $line );
+    }
+    
+    if ( $self->get_text_colour ne 'none' ) {
+        # work out where (indeed, if) the text fits
+        my( $twidth, $theight, $tdropheight, $point_size )
+            = $self->get_text_offset();
+    
+        if ( $twidth ) {
+            my $baseline   = $self->get_height() - $tdropheight;
+            my $text_total = $theight + $tdropheight;
+            my $remainder  = $self->get_height() - $text_total;
+        
+            my $tx = ( $self->get_width() - $twidth ) / 2;
+            my $ty = $baseline - int( $remainder / 2 );
+        
+            $image->stringFT(
+                    $self->{'_text'},
+                    $self->{'font'},
+                    $point_size,
+                    0,                      # angle
+                    $tx,
+                    $ty,
+                    $self->get_text()
+                );
+        }
+    }
+    
+    return $image->png;
 }
 
 method set_default_text {
@@ -58,6 +152,55 @@ method set_size_from_string ( Str $size ) {
     }
 }
 
+method get_text_offset {
+    my $x             = 0;
+    my $y             = 0;
+    my $point_size    = 10;
+    my $usable_width  = int( $self->get_width  * MAX_USABLE_RATIO );
+    my $usable_height = int( $self->get_height * MAX_USABLE_RATIO );
+    my @previous      = ( 0, 0, 0, 0 );
+    
+    while ( 1 ) {
+        my @bounds = GD::Image->stringFT(
+                $self->{'_line'},       # colour
+                $self->{'font'},
+                $point_size,
+                0,                      # angle
+                0,                      # x
+                0,                      # y
+                $self->get_text(),
+            );
+        
+        if ( @bounds ) {
+            my $text_width      = $bounds[2] - $bounds[0];
+            my $text_height     = 0 - $bounds[5];
+            my $text_dropheight = $bounds[1];
+            my $text_total_height = $text_height + $text_dropheight;
+            
+            # $text_width = $bounds[2] - $bounds[0];
+            # $text_height = $bounds[1] + ( 0 - $bounds[5]);
+            
+            my $too_big = ( $text_width > $usable_width )
+                          || ( $text_total_height > $usable_height );
+            
+            return @previous if $too_big;
+            
+            @previous =
+                ( $text_width, $text_height, $text_dropheight, $point_size );
+            $point_size += 5;
+        }
+        else {
+            return @previous;
+        }
+    }
+}
+
+method allocate_colour ( Str $colour, Int $alpha=0 ) {
+    my @rgb = $self->rgb_to_hex( $colour );
+    my $img = $self->{'_image'};
+    
+    return $img->colorAllocateAlpha( @rgb, $alpha )
+}
 method rgb_to_hex ( Str $hex ) {
     # TODO lookup standard colour values
     
