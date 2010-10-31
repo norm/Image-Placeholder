848db50400fc2c1fe45658ce4d0b3ce132c1e5ab
Mark Norman Francis <norm@cackhanded.net>
2010-10-31 23:06:58 +0000

    Test rgb values converting to hex.

--- p5-Image-Placeholder/lib/Image/Placeholder.pm
+++ p5-Image-Placeholder/lib/Image/Placeholder.pm
@@ -36,5 +36,22 @@ method set_default_text {
     my $size = sprintf '%s×%s', $self->get_width(), $self->get_height();
     $self->set_text( $size );
 }
+method rgb_to_hex ( Str $hex ) {
+    # TODO lookup standard colour values
+    
+    # must be a hex value
+    return( 0, 0, 0 )
+        unless $hex =~ m{^[0-9a-f]+$}i;
+    
+    # allow CSS style shorthands (f60 == ff6600)
+    $hex = "$1$1$2$2$3$3"
+        if $hex =~ m{^([0-9a-f])([0-9a-f])([0-9a-f])$}i;
+    
+    # must be six chars long
+    return( 0, 0, 0 )
+        unless 6 == length $hex;
+    
+    return map { hex($_) } unpack 'a2a2a2', $hex;
+}
 
 1;
